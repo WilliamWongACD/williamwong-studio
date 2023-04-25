0366df4769ae54d47aa45c228f9e152f4a940981
Ryan <ryancheta@users.noreply.github.com>
2023-04-25 07:02:23 -0700

    add style, string constants and use them

--- src/PageWrapper.jsx
+++ src/PageWrapper.jsx
@@ -4,6 +4,14 @@ import { Link } from 'react-router-dom';
 import WordMark from './components/WordMark';
 import { flex, centerItems } from './assets/_flex.module.scss';
 import { classes } from './utils/index';
+import CONSTANTS from './constants';
+
+const {
+  styles: {
+    spacing: { margin }
+  },
+  icons
+} = CONSTANTS;
 
 const navLinks = [
   { to: { pathname: '/', state: { shouldScrollToWork: true } }, title: 'Work' },
@@ -18,8 +26,10 @@ const PageWrapper = ({ light, children }) => {
   function handleScroll() {
     let hero = document.querySelector("[class*='hero']");
     let nav = document.querySelector('nav');
+    let heroText = document.getElementById('heroText');
     let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     nav.style.opacity = 1 - scrollTop / (hero.clientHeight - nav.clientHeight);
+    heroText.style.opacity = 1 - scrollTop / ((hero.clientHeight - nav.clientHeight) / 2);
   }
 
   React.useEffect(() => {
@@ -31,16 +41,16 @@ const PageWrapper = ({ light, children }) => {
     <div className={[styles.wrapper, light ? styles.light : ''].join(' ')}>
       <nav className={styles.nav}>
         <Link className={classes(flex, centerItems)} to={{ pathname: '/', state: { shouldScrollToTop: true } }} ref={wordMarkRef}>
-          <img src="http://williamwong.info/williamwong.design/assets/w2_logo.svg" />
+          <img src={icons.logo} />
           <h5 className={styles.wordMark}>&nbsp;&nbsp;William Wong</h5>
         </Link>
-        <div style={{ marginLeft: 'auto' }}>
+        <div style={margin.marginLeftAuto}>
           <Link
             to={{
               pathname: '/',
               state: { shouldScrollToWork: true }
             }}
-            style={{ marginRight: '2rem' }}
+            style={margin.marginRightMd}
           >
             Work
           </Link>
@@ -67,4 +77,5 @@ function Footer() {
     </footer>
   );
 }
+
 export default PageWrapper;
